37d50fd47232ce2dfc7576b7e3f33140d4677127
LiuFei <liufei.github@gmail.com>
2013-03-18 11:39:40 +0800

    Second commit in github

---
+++ Cat.java
@@ -0,0 +1,16 @@
+public class Cat {
+	private String eyeColor;
+	private int weight;
+	public String getEyeColor{
+		return this.eyeColor;
+	}
+	public void setEyeColor(String eyeColor){
+		this.eyeColor = eyeColor;
+	}
+	public int getWeight{
+		return this.weight;
+	}
+	public void setWeight(int weight) {
+		this.weight = weight;
+	}	
+}
